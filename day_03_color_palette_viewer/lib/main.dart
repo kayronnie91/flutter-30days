48a85545329ab6af5ffc55c7e3ed7f30c8206561
SkylinkTechnologies <kayronnie91@gmail.com>
2026-05-06 07:18:28 +0700

    Setup Stateful HomeScreen with PaletteColor model and initial state

--- day_03_color_palette_viewer/lib/main.dart
+++ day_03_color_palette_viewer/lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+class PaletteColor {
+  final String hex;
+  final String name;
+  final Color color;
+
+  PaletteColor({required this.color, required this.name, required this.hex});
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,9 +15,35 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late PaletteColor currentColor;
+  final List<PaletteColor> colors = [
+    PaletteColor(color: Colors.blue, name: 'Blue', hex: '#BAS44S3'),
+    PaletteColor(color: Colors.red, name: 'Red', hex: '#4464BAG'),
+    PaletteColor(color: Colors.green, name: 'Green', hex: '#GGFF7384'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    currentColor = colors[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
