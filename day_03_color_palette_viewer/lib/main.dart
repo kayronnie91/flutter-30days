207562b0b2e4a5d59937aaeb9c5cb07798be1e9a
SkylinkTechnologies <kayronnie91@gmail.com>
2026-05-13 15:51:51 +0700

    Build palette viewer card layout and collection UI

--- day_03_color_palette_viewer/lib/main.dart
+++ day_03_color_palette_viewer/lib/main.dart
@@ -45,7 +45,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // UI uses STATE
+      backgroundColor: const Color.fromARGB(
+        255,
+        240,
+        237,
+        237,
+      ), // UI uses STATE
       appBar: AppBar(
         leading: Icon(Icons.palette),
         title: Text('HueGallery'),
@@ -122,6 +127,89 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ],
                       ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Gallery Collections',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      '128 Colors',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 86, 79, 190),
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 22),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 2,
+                        spreadRadius: 0.5,
+                        offset: Offset(1, 2),
+                      ),
+                    ],
+                  ),
+                  height: 135,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            color: currentColor.color,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              currentColor.name,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Text(
+                              currentColor.hex,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 16),
+                        Spacer(),
+                        Checkbox(value: true, onChanged: (value) {}),
+                      ],
                     ),
                   ),
                 ),
